7c3f56d62248d276dcf569728bd542cb7bece053
John <john@real-currents.com>
2020-10-25 18:30:32 -0400

    start rusty checkers board

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-fetch('./target/wasm32-unknown-unknown/release/wasm_rust.wasm').then(response =>
+fetch('./target/wasm32-unknown-unknown/release/wasm_rusty-checkers.wasm').then(response =>
   response.arrayBuffer()
 ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
   instance = results.instance;
-  document.getElementById("container").textContent = instance.exports.add_one(41);
+  console.log(instance);
 }).catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,2 @@
 #[no_mangle]
-pub extern "C" fn add_one(x: i32) -> i32 {
-    x + 1
-}
+mod board;
