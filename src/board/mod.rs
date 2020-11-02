bdd51d070a0a469221234138e46b4c2f6e3f8b37
John <john@real-currents.com>
2020-11-02 07:22:08 -0500

    complete rusty checkers board structs

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -34,6 +34,40 @@ impl Coordinate {
         let Coordinate(x, y) = self;
         x <= 7 && y <= 7
     }
+
+    pub fn jump_targets_from(&self) -> impl Iterator<Item = Coordinate> {
+        let mut jumps = Vec::new();
+        let Coordinate(x, y) = *self;
+        if y >= 2 {
+            jumps.push(Coordinate(x + 2, y - 2));
+        }
+        jumps.push(Coordinate(x + 2, y + 2));
+
+        if x >= 2 && y >= 2 {
+            jumps.push(Coordinate(x - 2, y - 2));
+        }
+        if x >= 2 {
+            jumps.push(Coordinate(x - 2, y + 2));
+        }
+        jumps.into_iter()
+    }
+
+    pub fn move_targets_from(&self) -> impl Iterator<Item = Coordinate> {
+        let mut moves = Vec::new();
+        let Coordinate(x, y) = *self;
+        if x >= 1 {
+            moves.push(Coordinate(x - 1, y + 1));
+        }
+        moves.push(Coordinate(x + 1, y + 1));
+
+        if y >= 1 {
+            moves.push(Coordinate(x + 1, y - 1));
+        }
+        if x >= 1 && y >= 1 {
+            moves.push(Coordinate(x - 1, y - 1));
+        }
+        moves.into_iter()
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
