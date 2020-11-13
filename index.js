7576f0500784d275cecffb97771ff5b4ddd8bc92
dancingfrog <john@real-currents.com>
2020-11-13 07:59:43 -0500

    update index import of wasm module

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-fetch('./target/wasm32-unknown-unknown/release/wasm_rust.wasm').then(response =>
-  response.arrayBuffer()
-).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
-  instance = results.instance;
-  document.getElementById("container").textContent = instance.exports.add_one(41);
-}).catch(console.error);
+fetch('./target/wasm32-unknown-unknown/release/wasm_rust.wasm')
+    .then(response => response.arrayBuffer())
+    .then(instantiateWebAssembly)
+    .then(callWebAssemblyFunction)
+    .catch(e => {
+        console.error('Fallback to debug wasm module because:\n' + e);
+        fetch('./target/wasm32-unknown-unknown/debug/wasm_rust.wasm')
+            .then(response => response.arrayBuffer())
+            .then(instantiateWebAssembly)
+            .then(callWebAssemblyFunction)
+            .catch(console.error);
+    });
+
+function instantiateWebAssembly (bytes) {
+    return WebAssembly.instantiate(bytes);
+}
+
+function callWebAssemblyFunction (results) {
+    instance = results.instance;
+    document.getElementById("container").textContent = instance.exports.add_one(41);
+}
