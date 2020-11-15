d11f5a4cd14233b039a8a88830bed13586d5b0e5
John <john@real-currents.com>
2020-11-15 13:51:39 -0500

    complete game engine

--- src/game.rs
+++ src/game.rs
@@ -40,4 +40,80 @@ impl GameEngine {
                 self.board[x][y] = Some(GamePiece::new(PieceColor::Black));
             })
     }
+
+    fn legal_moves(&self) -> Vec<Move> {
+        let mut moves: Vec<Move> = Vec::new();
+
+        for col in 0..8 {
+            for row in 0..8 {
+                if let Some(piece) = self.board[col][row] {
+                    if piece.color == self.current_turn {
+                        let loc = Coordinate(col, row);
+                        let &mut vmoves: Vec<Move> = self.valid_moves_from(loc);
+                        moves.append(vmoves);
+                    }
+                }
+            }
+        }
+        moves
+    }
+
+    fn valid_moves_from(&self, loc: Coordinate) -> Vec<Moves> {
+        let Coordinate(x, y) = loc;
+
+        if let Some(p) = self.board[x][y] {
+            let mut jumps = loc
+                .jump_targets_from()
+                .filter(|t| self.valid_jump(&p, &loc, &t))
+                .map(|ref t| Move {
+                    from: loc.clone(),
+                    to: t.clone()
+                }).collect::<Vec<Move>>();
+            let mut moves = loc
+                .move_targets_from()
+                .filter(|t| self.valid_move(&p, &loc, &t))
+                .map(|ref t| Move {
+                    from: loc.clone(),
+                    to: t.clone()
+                }).collect::<Vec<Move>>();
+            jumps.append(&mut moves);
+            jumps
+
+        } else {
+            Vec::new()
+        }
+    }
+
+    pub fn move_piece(&mut self, mv: &Move) -> Result<MoveResult, ()> {
+        let legal_moves = self.local_moves();
+
+        if (!legal_moves.contains(mv)) {
+            return Err(());
+        } else {
+            let Coordinate(fx, fy) = mv.from;
+            let Coordinate(tx, ty) = mv.to;
+            let piece = self.board[fx][fy].unwrap();
+            let midpiece_coordinate = self.midpiece_coordinate(fx, fy, tx, ty);
+
+            if let Some(Coordinate(x, y)) = midpiece_coordiante {
+                self.board[x][y] = None;
+            }
+
+            self.board[tx][ty] = Some(piece);
+            self.board[fx][fy] = None;
+
+            let crowned = if self.should_crown(piece, mv.to) {
+                self.crown_piece(mv.to);
+                true
+            } else {
+                false
+            };
+            self.advance_turn();
+
+            Ok(MoveResult {
+                mv: mv.clone(),
+                crowned: crowned
+            })
+        }
+    }
 }
