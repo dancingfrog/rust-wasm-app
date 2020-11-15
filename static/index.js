4002f6a8187d92841f3948b025b9b050c22a0792
John <john@real-currents.com>
2020-11-15 08:37:06 -0500

    copy static file to public

---
+++ static/index.js
@@ -0,0 +1,21 @@
+fetch('wasm_rusty_checkers.wasm')
+    .then(response => response.arrayBuffer())
+    .then(instantiateWebAssembly)
+    .then(callWebAssemblyFunction)
+    .catch(e => {
+        console.error('Fallback to debug wasm module because:\n' + e);
+        fetch('wasm_rusty_checkers.wasm')
+            .then(response => response.arrayBuffer())
+            .then(instantiateWebAssembly)
+            .then(callWebAssemblyFunction)
+            .catch(console.error);
+    });
+
+function instantiateWebAssembly (bytes) {
+    return WebAssembly.instantiate(bytes);
+}
+
+function callWebAssemblyFunction (results) {
+    const instance = results.instance;
+    console.log(instance);
+}
